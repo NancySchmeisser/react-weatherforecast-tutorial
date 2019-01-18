e795adc7af7d8c07a9f8543708a71409cc0d76b0
NancySchmeisser <nancy.schmeisser@netzalist.de>
2019-01-18 08:24:10 +0100

    feat: del div

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import '/DayView.css';
+
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        Welcome to Weather Forecast 1.0
+     
       </div>
     );
   }
