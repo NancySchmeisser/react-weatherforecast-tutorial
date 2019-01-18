de4a25abe5ed398cf5bf38f77a76f08de86776c4
NancySchmeisser <nancy.schmeisser@netzalist.de>
2019-01-18 16:28:16 +0100

    feat: DayView.css, js

--- src/DayView.js
+++ src/DayView.js
@@ -7,9 +7,13 @@ import sunny from './sunny.png';
 class DayView extends Component {
   render() {
     return (
-      <div>
-     Welcome to Weather Forecast!
-     <img src='sunny.png' alt={sunny} width="50px" height="50px"/>
+        
+      <div class="weather">
+     
+     <div class="weekday">Mittwoch</div>
+     <img src={sunny} alt='sunny' width="100px" height="100px"/>
+     <div class="tempHigh">25°</div>
+     <div class="tempLow">17°</div>
       </div>
     );
   }
