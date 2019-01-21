ea65cd2b7164fb765bd4bbdadb062ac70184f758
NancySchmeisser <nancy.schmeisser@netzalist.de>
2019-01-21 10:15:39 +0100

    feat: change DayView.css, props DayView.js, add img,

--- src/DayView.js
+++ src/DayView.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react';
 import './DayView.css';
-import sunny from './sunny.png';
+import sunnyImg from './sunny.png';
+import cloudyImg from './cloudy.png';
+import rainyImg from './rainy.png';
+import hailImg from './hail.png';
+import snowyImg from './snowy.png';
 
 
 
 class DayView extends Component {
   render() {
+
+    let img = sunnyImg;
+    if (this.props.weather === "sunny") {
+      img = sunnyImg;
+    }
+    else if (this.props.weather === "cloudy") {
+      img = cloudyImg;
+    }
+    if (this.props.weather === "rain") {
+      img = rainyImg;
+    }
+    if (this.props.weather === "hail") {
+      img = hailImg;
+    }
+    if (this.props.weather === "snowy") {
+      img = snowyImg;
+    }
+
     return (
-        
+
       <div class="weather">
-     
-     <div class="weekday">Mittwoch</div>
-     <img src={sunny} alt='sunny' width="100px" height="100px"/>
-     <div class="tempHigh">25°</div>
-     <div class="tempLow">17°</div>
+
+        <div class="weekday">Mittwoch</div>
+        <img src={img} alt='sunny' width="100px" height="100px" /> {this.props.weather}
+        <div class="tempHigh">{this.props.highTemp}°</div>
+        <div class="tempLow">17°</div>
       </div>
     );
   }
